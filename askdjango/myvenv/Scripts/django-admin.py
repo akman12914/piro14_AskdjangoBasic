3a03cbe8f9115b5f5e13f59675981306b671f2a3
Yujin Sung <rnlduadns98@naver.com>
2021-01-19 01:49:19 +0900

    (중급)01 장고 프로젝트 생성 및 기본 프로젝트 구성

---
+++ askdjango/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rnldu\desktop\askdjango-s2-basic-ed459ca84a2b98d0ad374ea9e4c3059d46d68e6d\askdjango\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
