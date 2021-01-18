56cf453cc3003b808bfa62f0346992db4c9804eb
Yujin Sung <rnlduadns98@naver.com>
2021-01-19 03:06:16 +0900

    (중급)02 로그인 구현하기

--- askdjango/askdjango/urls.py
+++ askdjango/askdjango/urls.py
@@ -1,9 +1,11 @@
+from django import contrib
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-    path('',lambda req: redirect('/blog/')),
+    path('',lambda req: redirect('blog:post_list')),
 ]
